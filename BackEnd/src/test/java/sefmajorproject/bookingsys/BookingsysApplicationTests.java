a7e7ae3e79a6de8fa1e085f8de8988e6a1dc4c40
Theodore Politis <s3661671@student.rmit.edu.au>
2020-08-01 21:50:17 +1000

    Basic springboot setup from tuturial two

---
+++ BackEnd/src/test/java/sefmajorproject/bookingsys/BookingsysApplicationTests.java
@@ -0,0 +1,13 @@
+package sefmajorproject.bookingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
