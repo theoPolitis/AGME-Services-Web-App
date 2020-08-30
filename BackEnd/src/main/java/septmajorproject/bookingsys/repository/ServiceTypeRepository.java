cb39e15965edd8d49da9cd0c603e9304e4414d81
Theodore Politis <s3661671@student.rmit.edu.au>
2020-08-30 16:07:59 +1000

    Added service type tests for Ghaida

--- BackEnd/src/main/java/septmajorproject/bookingsys/repository/ServiceTypeRepository.java
+++ BackEnd/src/main/java/septmajorproject/bookingsys/repository/ServiceTypeRepository.java
@@ -6,4 +6,6 @@ import septmajorproject.bookingsys.model.ServiceType;
 public interface ServiceTypeRepository extends CrudRepository<ServiceType, Long> {
     @Override
     Iterable<ServiceType> findAllById(Iterable<Long> iterable);
+
+    ServiceType findByServiceName(String serviceName);
 }
