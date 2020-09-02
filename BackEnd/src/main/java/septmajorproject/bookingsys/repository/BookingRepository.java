ec3c7b65cb972e8424d22ffa6c2c1b5057dc77cf
s3781198 <s3781198@student.rmit.edu.au>
2020-09-02 21:43:22 +1000

    Finalised booking repository tests

--- BackEnd/src/main/java/septmajorproject/bookingsys/repository/BookingRepository.java
+++ BackEnd/src/main/java/septmajorproject/bookingsys/repository/BookingRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import septmajorproject.bookingsys.model.Booking;
 import septmajorproject.bookingsys.model.BookingPK;
+import septmajorproject.bookingsys.model.Customer;
+import septmajorproject.bookingsys.model.Employee;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @Repository
@@ -13,9 +16,11 @@ public interface BookingRepository extends JpaRepository<Booking, BookingPK> {
     //Further table functionality will be entered as needed
 
     @Override
-    List<Booking> findAllById(Iterable<BookingPK> id);
+    List<Booking> findAllById(Iterable<BookingPK> ids);
 
-    List<Booking> findByEmployee(String employee);
+    List<Booking> findAllByEmployee(Employee employee);
 
-    List<Booking> findByCustomer(String customer);
+    List<Booking> findAllByCustomer(Customer customer);
+
+    List<Booking> findAll();
 }
