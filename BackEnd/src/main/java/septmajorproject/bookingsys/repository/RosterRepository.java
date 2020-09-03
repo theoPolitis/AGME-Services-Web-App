e243aa6cce235db27f4b5768a03e488ea86b8e92
Muditha Kodithuwakku <s3813354@student.rmit.edu.au>
2020-09-03 10:50:39 +1000

    RosterRepo tests are done

--- BackEnd/src/main/java/septmajorproject/bookingsys/repository/RosterRepository.java
+++ BackEnd/src/main/java/septmajorproject/bookingsys/repository/RosterRepository.java
@@ -3,12 +3,26 @@ package septmajorproject.bookingsys.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import septmajorproject.bookingsys.model.Booking;
+import septmajorproject.bookingsys.model.Employee;
 import septmajorproject.bookingsys.model.Roster;
 import septmajorproject.bookingsys.model.RosterPK;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface RosterRepository extends JpaRepository<Roster, RosterPK> {
-    //Further table functionality will be entered as needed
+
+//    Employee employee = new Employee();
+
+    List<Roster> findAllByEmployee(Employee employee);
+
+    @Override
+    //returns all rosters
+    List<Roster> findAll();
+
+    List<Roster> getAllByEmployeeId(String employeeId);
+
+
 }
--- BackEnd/src/test/java/septmajorproject/bookingsys/rosterTest/RosterRepositoryIntegrationTest.java
+++ BackEnd/src/test/java/septmajorproject/bookingsys/rosterTest/RosterRepositoryIntegrationTest.java
@@ -2,16 +2,30 @@ package septmajorproject.bookingsys.rosterTest;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+import septmajorproject.bookingsys.model.Employee;
+import septmajorproject.bookingsys.model.Roster;
+import septmajorproject.bookingsys.model.RosterPK;
 import septmajorproject.bookingsys.repository.RosterRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class RosterRepositoryIntegrationTest {
+
+    Employee employeeOne = new Employee("1234", "Alex", "Test", "s3661671@student.rmit.edu.au", 0424735215, "Something", "s3661671", "password");
+    Employee employeeTwo = new Employee("5678", "Alma", "Tests", "s3661671@student.rmit.edu.au", 0424735215, "Something", "s3661671", "password");
+    Employee employeeThree = new Employee("3456", "Sam", "Rizzo", "s3661@student.rmit.edu.au", 0424735213, "Something", "s3661673", "password");
 
     @Autowired
     private TestEntityManager testEntityManager;
@@ -23,8 +37,59 @@ public class RosterRepositoryIntegrationTest {
     @Before
     public  void setUp(){
 
+        Roster rosterOne = new Roster(employeeOne,(new RosterPK((new Date(2020,8,27)),(new Time(12,30,0)))));
+        Roster rosterTwo = new Roster(employeeOne,(new RosterPK((new Date(2020,8,28)),(new Time(12,30,0)))));
+        Roster rosterThree = new Roster(employeeOne,(new RosterPK((new Date(2020,8,29)),(new Time(12,30,0)))));
+        Roster rosterFour = new Roster(employeeTwo,(new RosterPK((new Date(2020,8,30)),(new Time(12,30,0)))));
+        Roster rosterFive = new Roster(employeeTwo,(new RosterPK((new Date(2020,8,31)),(new Time(12,30,0)))));
+
+        testEntityManager.persist(employeeOne);
+        testEntityManager.persist(rosterOne);
+        testEntityManager.persist(rosterTwo);
+        testEntityManager.persist(rosterThree);
+
+        testEntityManager.flush();
+
     }
 
-    //pretty much test getting data
+    @Test
+    public void findAllRostersForTheEmployee_returnListRoster(){
+
+        List<Roster> rosterList = rosterRepository.findAllByEmployee(employeeOne);
+
+        assertThat(rosterList.size() == 3);
+    }
+
+    @Test
+    public void findAllRostersForNonExistingEmployee_returnNull()
+    {
+
+        List<Roster> rosterList = rosterRepository.findAllByEmployee(employeeThree);
+
+        assertThat(rosterList.size() == 0);
+
+    }
+
+    @Test
+    public void getAllRosters_returnListOfRosters()
+    {
+        List<Roster> rosterList = rosterRepository.findAll();
+
+        assertThat(rosterList.size() == 5);
+    }
+
+    @Test
+    public void getAllRosterByEmployeeId_returnListOfRosters(){
+        List<Roster> rosterList = rosterRepository.getAllByEmployeeId(employeeOne.getEmployeeIdentifier());
+
+        assertThat(rosterList.size() == 3);
+    }
+
+    @Test
+    public void getAllRosterByNonExistingEmployeeId_returnNull(){
+        List<Roster> rosterList = rosterRepository.getAllByEmployeeId(employeeOne.getEmployeeIdentifier());
+
+        assertThat(rosterList.size() == 0);
+    }
 
 }
