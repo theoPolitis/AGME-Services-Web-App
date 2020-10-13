b27dc440f33b1655d7125c975383e01ebf08dacf
[Jack Williams s3788167] <s3788167@student.rmit.edu.au>
2020-10-13 22:10:10 +1100

    PBI 35 completed, added the analytics page

--- FrontEnd/majorproject-front-end/src/Components/analytics/Analytics.js
+++ FrontEnd/majorproject-front-end/src/Components/analytics/Analytics.js
@@ -1,80 +1,131 @@
 import React, { Component } from "react";
 import Plotly from "plotly.js-basic-dist";
+import { Redirect } from "react-router-dom";
 import createPlotlyComponent from "react-plotly.js/factory";
-import Axios from "axios";
 const Plot = createPlotlyComponent(Plotly);
 
 
-class Analytics extends Component{
+class Analytics extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {};
-        Axios.get("http://localhost:8080/api/employee/all/"+props.user.serviceNo, {}).then(
-            (res) => {this.setState({employees: res.data},this.populateRoster(res.data))}
-        ).catch(
-            (error) => {
-                console.log(error);
-                alert("An error occured");
-            }
-        );
-    }
-
-    populateRoster = (employees) => {
-        var roster = employees.map((employee) => (employee.roster));
-        this.setState({rosters:roster});
-        var numAvailable = [0,0,0,0,0,0,0];
-        var i;
-        for(i=0; i<roster.length;i++)
+        console.log(props.bookingInfo.length);
+        var totalBookings = [0,0,0,0,0,0,0,0];
+        for(var i = 0; i < props.bookingInfo.length; i++)
         {
-            var rost = roster[i];
-            if(rost.sunday)
+            var custBookings = props.bookingInfo[i].dates;
+            for(var j = 0; j < custBookings.length; j++)
             {
-                numAvailable[0] +=1;
-            }
-            if(rost.monday)
-            {
-                numAvailable[1] +=1;
-            }
-            if(rost.tuesday)
-            {
-                numAvailable[2] +=1;
-            }
-            if(rost.wednesday)
-            {
-                numAvailable[3] +=1;
-            }
-            if(rost.thursday)
-            {
-                numAvailable[4] +=1;
-            }
-            if(rost.friday)
-            {
-                numAvailable[5] +=1;
-            }
-            if(rost.saturday)
-            {
-                numAvailable[6] +=1;
+                totalBookings[j] += custBookings[j];
             }
         }
-        console.log(numAvailable);
-        this.setState({rosterNumbers: numAvailable});
-    } 
+        this.state = {
+            totalBookings: totalBookings
+        };
 
+    }
+
+    plotData = () =>{
+        var data = [];
+        for(var i = 0; i < this.props.bookingInfo.length; i++)
+        {
+            data.push(
+            <div>
+                <Plot 
+                    data={[
+                        {
+                            x: this.props.dates,
+                            y: this.props.bookingInfo[i].dates,
+                            type: 'scatter',
+                        }
+                    ]}
+                    layout={{ 
+                        width:"100%", 
+                        height: 300, 
+                        title: "Total Bookings For " + this.props.bookingInfo[i].name + " This Week",
+                        font: {
+                            family: 'Lato',
+                            size: 14,
+                            color: '#2197da'
+                        },
+                        plot_bgcolor: "black",
+                        paper_bgcolor: "black"
+
+                    }} 
+                />
+            </div>
+            );
+            
+        }
+        return data;
+    }
 
     render() {
-        return(
-            <Plot
-        data={[
-          {
-            x: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
-            y: this.state.rosterNumbers,
-            type: 'bar'
-          }
-        ]}
-        layout={ {width: 320, height: 240, title: 'Staff Availabilities'} }
-      />
-        );
+        if (this.props.loggedInStatus === "LOGGED_IN") {
+            return (
+                <div className="container">
+                    <h1>Analytics</h1>
+                    <div style={{width:"100%",margin:"auto"}}>
+                        <Plot
+                            data={[
+                                {
+                                    x: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                                    y: this.props.rosterInfo,
+                                    type: 'bar'
+                                }
+                            ]}
+                            layout={{ 
+                                width:"100%", 
+                                height: 300, 
+                                title: 'Staff Availabilities',
+                                font: {
+                                    family: 'Lato',
+                                    size: 14,
+                                    color: '#2197da'
+                                },
+                                plot_bgcolor: "black",
+                                paper_bgcolor: "black"
+        
+                            }} 
+                        />
+                    </div>
+                    <div>
+                        <Plot
+                            data={[
+                                {
+                                    x: this.props.dates,
+                                    y: this.state.totalBookings,
+                                    type: 'scatter',
+                                    mode: 'lines+markers',
+                                    marker: {
+                                        color: "#2197da",
+                                        width:8
+                                    }
+                                }
+                            ]}
+                            layout={{ 
+                                width:"100%", 
+                                height: 300, 
+                                title: "Total Bookings This Week",
+                                font: {
+                                    family: 'Lato',
+                                    size: 14,
+                                    color: '#2197da'
+                                },
+                                plot_bgcolor: "black",
+                                paper_bgcolor: "black"
+        
+                            }} 
+                        />
+                    </div>
+                    {this.plotData()}
+                </div>
+
+            );
+        } else {
+            return <Redirect to={{ pathname: "/" }} />;
+        }
+
     }
 }
 export default Analytics;
